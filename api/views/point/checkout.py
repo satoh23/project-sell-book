a567bae393d2460e2bb760087722d573343c8c9f
tetsuya <satoh1529@gmail.com>
2021-08-09 20:34:12 +0900

    initial commit

--- api/views/point/checkout.py
+++ api/views/point/checkout.py
@@ -15,30 +15,33 @@ stripe.api_key = settings.STRIPE_SECRET_KEY
 @api_view(['POST'])
 def save_stripe_info(request):
     data = request.data
-    name = data['name']
-    email = data['email']
     purchase_id = data['purchase']
     article_id = data['article']
-    payment_method_id = data['payment_method_id']
+    amount = Detail.objects.values_list('amount', flat=True).get(pk=article_id)
 
-    customer_data = stripe.Customer.list(email=email).data
+    if amount >= 1:
+        name = data['name']
+        email = data['email']
+        payment_method_id = data['payment_method_id']
 
-    if len(customer_data) == 0:
-        customer = stripe.Customer.create(
-            name=name, email=email, payment_method=payment_method_id)
-    else:
-        customer = customer_data[0]
+        customer_data = stripe.Customer.list(email=email).data
 
-    try:
-        stripe.PaymentIntent.create(
-            customer=customer,
-            payment_method=payment_method_id,
-            currency='jpy',
-            amount=math.ceil(data['amount']*1.038),
-            confirm=True
-        )
-    except Exception:
-        return Response(status=status.HTTP_400_BAD_REQUEST, data={'message': '決済に失敗しました'})
+        if len(customer_data) == 0:
+            customer = stripe.Customer.create(
+                name=name, email=email, payment_method=payment_method_id)
+        else:
+            customer = customer_data[0]
+
+        try:
+            stripe.PaymentIntent.create(
+                customer=customer,
+                payment_method=payment_method_id,
+                currency='jpy',
+                amount=math.ceil(amount*1.038),
+                confirm=True
+            )
+        except Exception:
+            return Response(status=status.HTTP_400_BAD_REQUEST, data={'message': '決済に失敗しました'})
 
     purchase = CustomUser.objects.get(pk=purchase_id)
     article = Detail.objects.get(pk=article_id)
