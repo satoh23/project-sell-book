a86c7bed13f9bd66946c52d01b03b184cbe0cd73
tetsuya <satoh1529@gmail.com>
2021-08-13 18:12:58 +0900

    ページングの追加

--- config/settings.py
+++ config/settings.py
@@ -211,8 +211,6 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',
     ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 2,
 }
 
 # stripe設定
