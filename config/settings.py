0e845a3dfe9ab8dde3af7c506535aebd959a4f0c
tetsuya <satoh1529@gmail.com>
2021-08-13 17:35:56 +0900

    メディアファイルの設定

--- config/settings.py
+++ config/settings.py
@@ -211,6 +211,8 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100,
 }
 
 # stripe設定
